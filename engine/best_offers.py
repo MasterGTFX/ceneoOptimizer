6f7b194e64ff8b587195f35d8ba456834c30ce54
filipsblue <f1f4k0x12>
2019-12-05 19:23:08 +0100

    add same seller delivery discount

--- engine/best_offers.py
+++ engine/best_offers.py
@@ -3,7 +3,7 @@ from scraper import CeneoScraper
 item1 = CeneoScraper("iphone xs", offers_number=5, min_rating=0, min_reviews=0, min_price=3000)
 item2 = CeneoScraper("młotek", offers_number=5, min_rating=0, min_reviews=0, min_price=10)
 item3 = CeneoScraper("powerbank", offers_number=5, min_rating=0, min_reviews=0, min_price=100)
-item4 = CeneoScraper("zegarek", offers_number=5, min_rating=0, min_reviews=0, min_price=1000)
+item4 = CeneoScraper("nokia", offers_number=5, min_rating=0, min_reviews=0, min_price=1000)
 item5 = CeneoScraper("piekarnik", offers_number=5, min_rating=0, min_reviews=0, min_price=500)
 prod1 = item1.get_all_offers()
 prod2 = item2.get_all_offers()
@@ -11,49 +11,50 @@ prod3 = item3.get_all_offers()
 prod4 = item4.get_all_offers()
 prod5 = item5.get_all_offers()
 
-#sorted_prod1=sorted(prod1, key = lambda i: (i['price']+i['delivery_price']))
-print(prod1)
-print(prod2)
-print(prod3)
-print(prod4)
-print(prod5)
-
 lista=[prod1, prod2, prod3, prod4, prod5]
 
-def get_best_offers(lista):
+def get_best_offers(offers_list):
 
-    best_offers1 = []
-    best_offers2 = []
-    best_offers3 = []
+    first_set = []
+    second_set = []
+    third_set = []
 
-    suma1 = 0.0
-    suma2 = 0.0
-    suma3 = 0.0
+    first_set_price = 0.0
+    second_set_price = 0.0
+    third_set_price = 0.0
 
-    zestawy = [best_offers1, best_offers2, best_offers3]
-
-    for item in lista:
+    for item in offers_list:
         item = sorted(item, key = lambda i: (i['price']+i['delivery_price']))
 
-    for item in lista:
-        best_offers1.append(item[0])
-        best_offers2.append(item[1])
-        best_offers3.append(item[2])
+    for item in offers_list:
+        first_set.append(item[0])
+        second_set.append(item[1])
+        third_set.append(item[2])
 
-    for oferta in best_offers1:
-        suma1 = suma1 + oferta['price'] + oferta['delivery_price']
+    sets = [first_set, second_set, third_set]
 
-    for oferta in best_offers2:
-        suma2 = suma2 + oferta['price'] + oferta['delivery_price']
-
-    for oferta in best_offers3:
-        suma3 = suma3 + oferta['price'] + oferta['delivery_price']
-
-
-    print("zestaw 1: koszt: ", suma1, best_offers1)
-    print("zestaw 2: koszt: ", suma2, best_offers2)
-    print("zestaw 3: koszt: ", suma3, best_offers3)
+    for set in sets:
+        for i in range(0, len(set)):
+            inc = 0
+            discount = 0.0
+            for j in range(1, len(set) - 2):
+                if set[i]['seller_name'] == set[j]['seller_name']:
+                    inc+=1
+            if inc:
+                set[i]['price'] = set[i]['price'] - inc * set[i]['delivery_price']
 
 
 
-get_best_offers(lista)
+    for offer in first_set:
+        first_set_price = first_set_price + offer['price'] + offer['delivery_price']
+
+    for offer in second_set:
+        second_set_price = second_set_price + offer['price'] + offer['delivery_price']
+
+    for offer in third_set:
+        third_set_price = third_set_price + offer['price'] + offer['delivery_price']
+
+    best_offers = [first_set, first_set_price, second_set, second_set_price, third_set, third_set_price]
+    return best_offers
+
+print(get_best_offers(lista))
